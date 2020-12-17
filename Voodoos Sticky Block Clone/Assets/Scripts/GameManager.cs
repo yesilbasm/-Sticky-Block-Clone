bfbbbcbea5a7dcf2707d77be2b0250020b2aab62
Mustafa Zübeyr Yeşilbaş <mustafazubeyryesilbas@gmail.com>
2020-12-17 04:19:16 +0300

    Final GamePlay

---
+++ Voodoos Sticky Block Clone/Assets/Scripts/GameManager.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+  public bool isGameOver;
+  public void RestartGame()
+  {
+      if(isGameOver)
+      {
+          SceneManager.LoadScene(0);
+      }
+  }
+}
---
+++ Voodoos Sticky Block Clone/Assets/Scripts/Movement.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    [SerializeField] private  PlayerManager playerManager;
+    public float movementSpeed;
+    public float controlSpeed;
+
+    private void FixedUpdate() {
+        
+        Move();
+    }
+
+   private void Move()
+   {
+       transform.position+=Vector3.forward*movementSpeed*Time.fixedDeltaTime;
+       if(Input.GetMouseButton(0))
+       {
+           transform.position+=Vector3.left*controlSpeed*Time.fixedDeltaTime;
+       }
+       else if(Input.GetMouseButton(1)) 
+       {
+           transform.position+=Vector3.right*controlSpeed*Time.fixedDeltaTime;
+       }
+   }
+}
